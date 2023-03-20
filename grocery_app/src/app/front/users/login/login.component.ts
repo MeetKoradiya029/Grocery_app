0969af8f1ada235247716d319eb22ae52ca4ec38
KoradiyaMeet029 <mvkoradiya1112@gmail.com>
2023-03-20 18:44:33 +0530

    validation added in forms

--- grocery_app/src/app/front/users/login/login.component.ts
+++ grocery_app/src/app/front/users/login/login.component.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-    loginForm:FormGroup;
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginForm = new FormGroup({
-      email:new FormControl('',Validators.required),
-      password:new FormControl('',Validators.required)
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
   }
 
-  submit(){
-    console.log(this.loginForm.value);
-  }
+  submit() {
+    let formData = this.loginForm.getRawValue();
 
+    let email = formData.email;
+    let emailRegex = '^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$';
+    let password = formData.password;
+    let flag = true;
+
+    if (!email || !email.match(emailRegex)) {
+      alert('invalid email or password');
+      return (flag = false);
+    } else if (password.length < 8) {
+      alert('invalid email or password');
+      return (flag = false);
+    }
+
+    if (flag == true) {
+      console.log('FormData : ', formData);
+    }
+
+    return flag;
+  }
 }
--- grocery_app/src/app/front/users/registration/registration.component.ts
+++ grocery_app/src/app/front/users/registration/registration.component.ts
@@ -1,32 +1,82 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  MinLengthValidator,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit{
+export class RegistrationComponent implements OnInit {
+  registerForm: FormGroup;
 
-  registerForm:FormGroup;
+  constructor() {}
 
-  constructor(){
+  ngOnInit() {
+    this.formInitialize();
   }
-
-  ngOnInit(){
+  formInitialize() {
     this.registerForm = new FormGroup({
-      firstname:new FormControl('',Validators.required),
-      lastname:new FormControl("",Validators.required),
-      email:new FormControl('',[Validators.required,Validators.email]),
-      country:new FormControl('',Validators.required),
-      address:new FormControl('',Validators.required),
-      city:new FormControl('',Validators.required),
-      state:new FormControl('',Validators.required),
-      zipcode:new FormControl('',[Validators.required,Validators.minLength(6)]),
-      
-    })
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        // Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
+      ]),
+      mobile: new FormControl('', [Validators.required]),
+      country: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      zipcode: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
-  formHandler(){
-    console.log("FormData"+this.registerForm.value);
+
+  formHandler() {
+    let flag = true;
+    let {
+      firstname,
+      lastname,
+      email,
+      password,
+      country,
+      address,
+      city,
+      state,
+      zipcode,
+      mobile,
+    } = this.registerForm.getRawValue();
+
+    if (
+      !firstname ||
+      !lastname ||
+      !email ||
+      !password ||
+      !country ||
+      !address ||
+      !city ||
+      !state ||
+      !zipcode ||
+      !mobile
+    ) {
+      alert('Complete form !!');
+      return (flag = false);
+    }
+
+    if (flag == true) {
+
+      console.log('firstname', this.registerForm.getRawValue());
+    }
+    return flag;
   }
 }
